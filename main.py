6707d01ab1183083a87651d9daf569e6c68b8162
yousefsul <67277838+yousefsul@users.noreply.github.com>
2021-03-25 01:05:15 +0200

    define read Patients list class structure

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+import logging
+from readPatientsList import ReadPatientsList
+# ? formatting the logging file  
+logging_format = "%(asctime)s =>  %(message)s"
+logging.basicConfig(filename="PatientsList.log", filemode="w", format=logging_format, level=logging.INFO,
+                    datefmt="%H:%M:%S")
+
+# ? add paitnets file name 
+EXCEL_FILE_NAME_DR_AFROZA_PATIENTS_LIST = "Dr.Afroza_PatientList_03162021.xls"
+
 """
 * Main Method 
 """
 if __name__ == '__main__':
-    print("initial commit")
-
-
-
+    try:
+        logging.info("Main Method:          Open DR_AfrozaAhmed excel sheet to read patients data")
+    except FileNotFoundError:
+        print("Main Method:", FileNotFoundError)
+        logging.error("Main Method:         DR_AfrozaAhmed patient excel file doesn't exist")
---
+++ readPatientsList.py
@@ -0,0 +1,6 @@
+class ReadPatientsList:
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def get_patients_list(self):
+        pass
